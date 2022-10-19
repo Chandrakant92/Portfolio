5214f48fc9c7a5305aaa993fb3897e44a8482021
Chandrakant <chandrakantgawali92@gmail.com>
2022-10-19 16:57:22 +0530

    Second commit

--- pages/Component/Services.js
+++ pages/Component/Services.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from '../../styles/Services.module.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCode, faComputer, faWeb} from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
 
 
 const Services = () => {
@@ -18,19 +19,19 @@ const Services = () => {
                         <p className={styles.pp1}> Web Development</p>
                         <p className={styles.pp2}>Fruit saw for brought fish forth had ave is man a that their Two he is dominion evening their Fruit saw for brought fish forth
                         </p>
-                        <p className={styles.pp3}> <a href="/">Learn More</a></p>
+                        <p className={styles.pp3}><Link href="/">Learn More</Link></p>
                     </div>
                     <div className={styles.box}>
                         <p className={styles.pp1}> Apps Design</p>
                         <p className={styles.pp2}>Fruit saw for brought fish forth had ave is man a that their Two he is dominion evening their Fruit saw for brought fish forth
                         </p>
-                        <p className={styles.pp3}> <a href="/">Learn More</a></p>
+                        <p className={styles.pp3}> <Link href="/">Learn More</Link></p>
                     </div>
                     <div className={styles.box}>
                         <p className={styles.pp1}> UI/UX Design</p>
                         <p className={styles.pp2}>Fruit saw for brought fish forth had ave is man a that their Two he is dominion evening their Fruit saw for brought fish forth
                         </p>
-                        <p className={styles.pp3}> <a href="/">Learn More</a></p>
+                        <p className={styles.pp3}><Link href="/">Learn More</Link></p>
                     </div>
                 </div>
             </div>
